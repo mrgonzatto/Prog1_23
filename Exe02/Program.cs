63884adaa3f8cbf766a48b43c0db2831c792f2e5
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2023-03-06 22:09:11 -0300

    Exercicio 2

---
+++ Exe02/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Olá, seja bem-vindo!");
+Console.WriteLine("Qual é o seu nome?");
+string name = Console.ReadLine();
+Console.WriteLine(
+    $"Olá {name} prazer conhecê-lo."
+);
+
+// Declarar sem inicializar / primitivo
+string message1;
+// Declarar inicalizado / abstrato
+String message2 = "Seu Nome Completo";
+// Declarar string vazia
+string message3 = String.Empty;
+string message4 = "";
+
+//Declaração com tipo implícito
+var message5 = "Mensagem aleatória";
+
+message1 = 
+"Rogério Ceni;Pelé;The Rock;Roberto Carlos";
+var palavras = message1.Split(';');
+foreach(var word in palavras)
+{
+    // Variável de escopo local do
+    // laço de repetição foreach
+    int a = 10; 
+    Console.WriteLine(word);
+}
+
+
+
+
+
