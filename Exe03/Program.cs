e44090ba404c65fef76c0c88ba04f126e5141d70
Mauricio Gonzatto <mauriciogonzatto@hotmail.com>
2023-03-13 21:47:58 -0300

    Aula string numeros e conversão

---
+++ Exe03/Program.cs
@@ -0,0 +1,19 @@
+﻿static void FormatString(
+    string stringToFormat    
+) {
+    stringToFormat.Replace("Mundo", "Marte");
+}
+
+static string FormatReturningString(string stringToFormat)
+{
+    return stringToFormat.Replace("Mundo", "Marte");
+}
+
+var mensagem = "Olá Mundo!";
+// Invocando o método sem retorno
+FormatString(mensagem);
+Console.WriteLine(mensagem);
+
+var outraMensagem = "Bom-dia Mundo!";
+Console.WriteLine( FormatReturningString(outraMensagem) );
+
---
+++ Exe04/Program.cs
@@ -0,0 +1,18 @@
+﻿var myFloat = 10f;
+float meuFloat = 11;
+
+// Conversão de tipo numérico
+int myInteger = 100;
+meuFloat = (float)myInteger;
+decimal myDecimal = Convert.ToDecimal(myInteger);
+
+var myDouble = 10d;
+myDecimal = 10m;
+
+string meuNumeroTexto = Convert.ToString(myInteger);
+int qtdChars = meuNumeroTexto.Length;
+for(int i = 0; i < qtdChars; i++)
+{
+    Console.WriteLine( meuNumeroTexto[i] );
+}
+
